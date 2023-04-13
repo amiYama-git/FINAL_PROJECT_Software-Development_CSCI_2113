f7a18fbdf0dca723fb12d2bd19a341aa46c5deb2
Juliet Wield <julietwield@Juliets-MacBook-Pro.local>
2023-04-13 17:13:20 -0400

    empty classes

---
+++ Card.java
@@ -0,0 +1,8 @@
+public class Card {
+	private int number; // 0 - 9 is a normal card; 
+	private char color; // r, y, b, g are normally colored cards; wild card is s (for special)
+
+	public Card () {
+		
+	}
+}
