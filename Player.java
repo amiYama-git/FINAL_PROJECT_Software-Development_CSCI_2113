76c6c49f14fd7f805b6d2e67bcf2748c17d4240b
Juliet Wield <julietwield@Juliets-MacBook-Pro.local>
2023-04-14 12:07:56 -0400

    attendance from first meeting

--- Player.java
+++ Player.java
@@ -3,6 +3,32 @@
  * Will store: card on top of deck, their own hand, how many cards the opponent has, their own name
  */
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class Player {
+	private ArrayList<Card> hand = new ArrayList<Card>();
+	static Card onStack;
+	
+
+	public Player (String host, int port, String name, UnoGUI gui) {
+
+	}
+
+	public Card drawCard() {
+		return null;
+	}
+
+	public void playCard(int number, char color) {
+		// check color first
+	}
+
+	public void receiveCard(Card card) {
+
+	}
+
+
+	
 	
 }
--- UnoServer.java
+++ UnoServer.java
@@ -1,8 +1,15 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 /*
  * This is the server
  * Stores player objects, stack of cards played (only the top one is important), the available cards to pull, base deck
  */
 
 public class UnoServer {
+	private ArrayList<Card> baseDeck = new ArrayList<Card>();
+
+	private ServerSocket server;
 	
 }
