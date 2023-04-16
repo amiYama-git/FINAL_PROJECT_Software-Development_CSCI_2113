22b64ffcbd9c6286be1127addc135469b331ca31
MiguelJoseSolis <122939255+MiguelJoseSolis@users.noreply.github.com>
2023-04-16 13:28:32 -0400

    Update Card.java
    Copying and Pasting the constructor in git because I can't figure out how to update the file with the changes rn without deleting everything :-(.

--- Card.java
+++ Card.java
@@ -2,7 +2,19 @@ public class Card {
 	private int number; // 0 - 9 is a normal card; 
 	private char color; // r, y, b, g are normally colored cards; wild card is s (for special)
 
-	public Card () {
-		
+	// Card Constructor
+	public Card (int number, char color) {
+		this.number = number;
+		this.color = color;
+	}
+	
+	// Number Getter
+	public int getNum () {
+		return number;
+	}
+
+	// Color Getter
+	public char getCol () {
+		return color;
 	}
 }
