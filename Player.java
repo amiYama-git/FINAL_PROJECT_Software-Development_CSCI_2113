c76399f99a3f3fb6ce5e57a2c6cd5c7837e70512
Juliet Wield <julietwield@Juliets-MacBook-Pro.local>
2023-04-17 11:36:20 -0400

    more notes

--- Player.java
+++ Player.java
@@ -4,75 +4,77 @@
  */
 
 
- import java.io.*;
- import java.net.*;
- import java.util.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
  
- public class Player {
-	 private ArrayList<Card> hand = new ArrayList<Card>();
-	 private static Card onStack;
-	 private Socket sock;
-	 private String name;
-	 private UnoGUI gui;
+public class Player {
+	private ArrayList<Card> hand = new ArrayList<Card>();
+	private static Card onStack;
+	private Socket sock;
+	private String name;
+	private UnoGUI gui;
+	private ObjectInputStream objectFromServer;
+	private BufferedReader br;
+	private ObjectOutputStream objectToServer;
+	private PrintWriter pw;
  
-	 public Player (String host, int port, String name, UnoGUI gui) {
-		 this.name = name;
-		 this.gui = gui;
+	public Player (String host, int port, String name, UnoGUI gui) {
+		this.name = name;
+		this.gui = gui;
 		 
-		 try {
-			 sock = new Socket(host, port);
-		 }
-		 catch (Exception e) {
-			 // error pop-up/message
-		 }
- 
-		 // start listening to the server
-		 (new listeningThread(sock)).start();
-	 }
- 
-	 public void drawCard() {
-		 // requests a card from the server
-	 }
- 
-	 // triggered by the GUI
-	 public void playCard(Card card) {
-		 int cardNum = card.getNumber();
-		 char cardCol = card.getColor();
- 
-		 // wild cards always get played
-		 if (cardCol == 's') {
-			 // send to the server
-		 }
- 
-		 // if the colors match
-		 else if (cardCol == onStack.getCol()) {
-			 // send to the server
-		 }
- 
-		 // if the numbers match
-		 else if (cardNum == onStack.getNum()) {
-			 // send to the server
-		 }
- 
-		 else {
-			 // pop-up telling the player that card isn't playable?
-		 }
-	 }
-   
-   // Or, since the GUI already has access to the ArrayList in order to present it, it can just send the number in the array that is clicked?
-	public void playCard(int played) {
-		if (canPlay(hand.get(played)) == true) {
-			// Sends the Card to the Network (Recieved back from Network to update top of Stack)
-		} else {
-			// GUI popup says "You can't do that!!!"
+		try {
+			sock = new Socket(host, port);
 		}
+		catch (Exception e) {
+			// error pop-up/message
+		}
+ 
+		// start listening to the server
+		(new listeningThread(sock)).start();
 	}
  
+	public void drawCard() {
+		// requests a card from the server
+	}
+ 
+	// triggered by the GUI
+	public boolean playCard(int num, char color) {
+		// wild cards always get played
+		if (color == 's') {
+			// check what kind of wild card it is
+			// do the appropriate things
+			// send to the server
+			// remove from hand
+			return true;
+		}
+ 
+		// if the colors match
+		else if (color == onStack.getCol()) {
+			// send to the server
+			// remove from hand
+			return true;
+		}
+ 
+		// if the numbers match
+		else if (num == onStack.getNum()) {
+			// send to the server
+			// remove from hand
+			return true;
+		}
+ 
+		else {
+			// pop-up telling the player that card isn't playable?
+			return false;
+		}
+	}
+
 	 public void receiveCard(Card card) {
-		 hand.add(card);
+		hand.add(card);
+		// send to the gui
 	 }
    
-   // Updates the Stack with the Card Recieved. 
+	// Updates the Stack with the Card Recieved. 
 	public void updateStack(Card card) {
 		onStack = card;
 	}
@@ -86,42 +88,43 @@
 		}
 	}
  
-	 private class listeningThread extends Thread {
-		 private Socket sock;
-		 private ObjectInputStream objectFromServer;
-		 private BufferedReader br;
-		 private ObjectOutputStream objectToServer;
-		 private PrintWriter pw;
+	private class listeningThread extends Thread {
+		private Socket sock;
+		private ObjectInputStream objectFromServer;
+		private BufferedReader br;
+		private ObjectOutputStream objectToServer;
+		private PrintWriter pw;
 		 
-		 public listeningThread (Socket sock) {
-			 this.sock = sock;
+		public listeningThread (Socket sock) {
+			this.sock = sock;
  
-			 // set up the i/o stream readers
-			 try {
-				 // read FROM the server
-				 objectFromServer = new ObjectInputStream(sock.getInputStream());
-				 br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			// set up the i/o stream readers
+			try {
+				// read FROM the server
+				objectFromServer = new ObjectInputStream(sock.getInputStream());
+			 	br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
  
-				 // write TO the server
+				// write TO the server
+				objectToServer = new ObjectOutputStream(sock.getOutputStream());
 				 
-			 }
-			 catch (Exception e) {
-				 // error pop-up/message
-			 }
-		 }
+			}
+			catch (Exception e) {
+				// error pop-up/message
+			}
+		}
  
-		 @Override
-		 public void run() {
-			 // whatever it does with the things the server is saying/sending
-			 try {
-				 while(true){ 
+		@Override
+		public void run() {
+			// whatever it does with the things the server is saying/sending
+			try {
+				while(true){ 
  
-				 }
-			 }
-			 catch (Exception e) {
-				 // error pop-up/message
-			 }
-		 }
-	 }
+				}
+			}
+			catch (Exception e) {
+				// error pop-up/message
+			}
+		}
+	}
 	 
- }
+}
