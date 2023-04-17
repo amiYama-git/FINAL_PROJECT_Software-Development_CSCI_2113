b1f13fb5fc6bd1f2d488e786653dc8ee95aaf9b8
Juliet Wield <julietwield@Juliets-MacBook-Pro.local>
2023-04-17 11:44:28 -0400

    notes on cards

--- Card.java
+++ Card.java
@@ -1,5 +1,5 @@
 public class Card {
-	private int number; // 0 - 9 is a normal card; -2 is plus two cards; -4 is plus 4 cards, -1 is change color
+	private int number; // 0 - 9 is a normal card; -2 is plus two cards; -4 is plus 4 cards, -1 is change color, -3 skip, -5 rotate
 	private char color; // r, y, b, g are normally colored cards; wild card is s (for special)
 
 	// Card Constructor
