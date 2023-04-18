a97ea4b48c8ecf425c311a17699d5f2d8c143b4c
MiguelJoseSolis <themiguelsolis@aol.com>
2023-04-18 12:39:25 -0400

    Cards are Added :-)

--- Deck.java
+++ Deck.java
@@ -50,8 +50,144 @@ public class Deck {
         private void addCards() {
 		// Adding the Cards (Pain!)
 
+		// 0
 		deck.add(new Card (0, 'r'));
 		deck.add(new Card (0, 'y'));
+		deck.add(new Card (0, 'g'));
+                deck.add(new Card (0, 'b'));
+
+		// 1
+		deck.add(new Card (1, 'r'));
+                deck.add(new Card (1, 'y'));
+                deck.add(new Card (1, 'g'));
+                deck.add(new Card (1, 'b'));
+                deck.add(new Card (1, 'r'));
+                deck.add(new Card (1, 'y'));
+                deck.add(new Card (1, 'g'));
+                deck.add(new Card (1, 'b'));
+
+		// 2
+                deck.add(new Card (2, 'r'));
+                deck.add(new Card (2, 'y'));
+                deck.add(new Card (2, 'g'));
+                deck.add(new Card (2, 'b'));
+                deck.add(new Card (2, 'r'));
+                deck.add(new Card (2, 'y'));
+                deck.add(new Card (2, 'g'));
+                deck.add(new Card (2, 'b'));
+
+		// 3
+                deck.add(new Card (3, 'r'));
+                deck.add(new Card (3, 'y'));
+                deck.add(new Card (3, 'g'));
+                deck.add(new Card (3, 'b'));
+                deck.add(new Card (3, 'r'));
+                deck.add(new Card (3, 'y'));
+                deck.add(new Card (3, 'g'));
+                deck.add(new Card (3, 'b'));
+
+		// 4
+                deck.add(new Card (4, 'r'));
+                deck.add(new Card (4, 'y'));
+                deck.add(new Card (4, 'g'));
+                deck.add(new Card (4, 'b'));
+                deck.add(new Card (4, 'r'));
+                deck.add(new Card (4, 'y'));
+                deck.add(new Card (4, 'g'));
+                deck.add(new Card (4, 'b'));
+
+		// 5
+                deck.add(new Card (5, 'r'));
+                deck.add(new Card (5, 'y'));
+                deck.add(new Card (5, 'g'));
+                deck.add(new Card (5, 'b'));
+                deck.add(new Card (5, 'r'));
+                deck.add(new Card (5, 'y'));
+                deck.add(new Card (5, 'g'));
+                deck.add(new Card (5, 'b'));
+
+		// 6 
+                deck.add(new Card (6, 'r'));
+                deck.add(new Card (6, 'y'));
+                deck.add(new Card (6, 'g'));
+                deck.add(new Card (6, 'b'));
+                deck.add(new Card (6, 'r'));
+                deck.add(new Card (6, 'y'));
+                deck.add(new Card (6, 'g'));
+                deck.add(new Card (6, 'b'));
+
+		// 7 
+                deck.add(new Card (7, 'r'));
+                deck.add(new Card (7, 'y'));
+                deck.add(new Card (7, 'g'));
+                deck.add(new Card (7, 'b'));
+                deck.add(new Card (7, 'r'));
+                deck.add(new Card (7, 'y'));
+                deck.add(new Card (7, 'g'));
+                deck.add(new Card (7, 'b'));
+
+		// 8 
+                deck.add(new Card (8, 'r'));
+                deck.add(new Card (8, 'y'));
+                deck.add(new Card (8, 'g'));
+                deck.add(new Card (8, 'b'));
+                deck.add(new Card (8, 'r'));
+                deck.add(new Card (8, 'y'));
+                deck.add(new Card (8, 'g'));
+                deck.add(new Card (8, 'b'));
+
+		// 9 
+                deck.add(new Card (9, 'r'));
+                deck.add(new Card (9, 'y'));
+                deck.add(new Card (9, 'g'));
+                deck.add(new Card (9, 'b'));
+                deck.add(new Card (9, 'r'));
+                deck.add(new Card (9, 'y'));
+                deck.add(new Card (9, 'g'));
+                deck.add(new Card (9, 'b'));
+
+		// Skip
+                deck.add(new Card (-3, 'r'));
+                deck.add(new Card (-3, 'y'));
+                deck.add(new Card (-3, 'g'));
+                deck.add(new Card (-3, 'b'));
+                deck.add(new Card (-3, 'r'));
+                deck.add(new Card (-3, 'y'));
+                deck.add(new Card (-3, 'g'));
+                deck.add(new Card (-3, 'b'));
+
+		// Rotate
+                deck.add(new Card (-5, 'r'));
+                deck.add(new Card (-5, 'y'));
+                deck.add(new Card (-5, 'g'));
+                deck.add(new Card (-5, 'b'));
+                deck.add(new Card (-5, 'r'));
+                deck.add(new Card (-5, 'y'));
+                deck.add(new Card (-5, 'g'));
+                deck.add(new Card (-5, 'b'));
+
+		// Plus Two
+                deck.add(new Card (-2, 'r'));
+                deck.add(new Card (-2, 'y'));
+                deck.add(new Card (-2, 'g'));
+                deck.add(new Card (-2, 'b'));
+                deck.add(new Card (-2, 'r'));
+                deck.add(new Card (-2, 'y'));
+                deck.add(new Card (-2, 'g'));
+                deck.add(new Card (-2, 'b'));
+
+		// WILD CARD!!!! :-)
+		deck.add(new Card (-1, 's'));
+                deck.add(new Card (-1, 's'));
+                deck.add(new Card (-1, 's'));
+                deck.add(new Card (-1, 's'));
+
+		// WILD Plus Four
+		deck.add(new Card(-4, 's'));
+                deck.add(new Card(-4, 's'));
+                deck.add(new Card(-4, 's'));
+                deck.add(new Card(-4, 's'));
+
 
 		// Shuffling the Deck
 		shuffle();
