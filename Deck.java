3a9a01eea77f501a69bb46dd41e50e6c50c820b8
MiguelJoseSolis <themiguelsolis@aol.com>
2023-04-18 12:05:35 -0400

    Methods

--- Deck.java
+++ Deck.java
@@ -1,22 +1,39 @@
 
+import java.util.*;
 
 public class Deck {
 
-	// Holds all 108 Cards in Game - has
+	// Holds all 108 Cards in Game
 	private ArrayList <Card> deck;
 
 	public Deck() {
 		addCards();
 	}
 
-	private addCards() {
+	        // Draws the Top Card from the Deck. 
+        public Card drawCard() {
 
+                // If the Deck is Empty, then it adds all of the cards back into the Deck and ReShuffles
+                if (deck.isEmpty() == true) {
+                        addCards();
+                }
+                // Removes a Card from Deck and Returns it
+                int size = deck.size();
+                Card card = deck.get(size);
+                deck.remove(size);
+                return card;
+        }
+
+	// Shuffles the Deck
+	private void shuffle() {
+		Collections.shuffle(deck);
 	}
 
-	public Card drawCard() {
-		if (deck.size == 0) {
-			addCards();
-		}
-		// Sends Card to the Server
-	}
+        // Adds all 108 Cards to the Deck, then shuffles.
+        private void addCards() {
+		// Adding the Cards (Pain!)
+
+		// Shuffling the Deck
+		shuffle();
+        }
 }
